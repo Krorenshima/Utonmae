4629a938f162f9faf3a89d41649e138ef6ec3026
Krorenshima <official.chubbyroo@gmail.com>
2021-02-05 03:23:10 -0800

    hm

--- bump-v.js
+++ bump-v.js
@@ -1,17 +1,25 @@
 (function () {
-  let fs, pkg, vers;
+  let fs, fetch, pkg, vers;
   fs = require('fs');
+  fetch = require('node-fetch');
   pkg = require('./package.json');
-  vers = pkg.version.split('.').map(n => +n);
-  vers[vers.length - 1]++;
-  if (vers[vers.length - 1] > 9) {
-    vers[vers.length - 1] = 0;
-    vers[vers.length - 2]++
-  }
-  if (vers[vers.length - 2] > 9) {
-    vers[vers.length - 2] = 0;
-    vers[vers.length - 3]++
-  }
-  pkg.version = vers.join('.');
-  fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2), 'utf8')
+  fetch('https://cdn.jsdelivr.net/npm/utonmae@latest/package.json')
+  .then(resp => resp.json())
+  .then(info => {
+    if (info.version === pkg.version) {
+      return
+    }
+    vers = pkg.version.split('.').map(n => +n);
+    vers[vers.length - 1]++;
+    if (vers[vers.length - 1] > 9) {
+      vers[vers.length - 1] = 0;
+      vers[vers.length - 2]++
+    }
+    if (vers[vers.length - 2] > 9) {
+      vers[vers.length - 2] = 0;
+      vers[vers.length - 3]++
+    }
+    pkg.version = vers.join('.');
+    fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2), 'utf8')
+  })
 })();
