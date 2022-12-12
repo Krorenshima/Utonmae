96beaba4938aad509209667347996c1500d58799
Krorenshima <official.chubbyroo@gmail.com>
2022-12-12 09:45:01 -0800

    upgred
    an upgred to be less bad
    bad code = u---------------------g---------------------g---------------------h---------------------h---------------------h---------------------A---------------------A---------------------A-
    good code = *n y o o m*

--- bump-v.js
+++ bump-v.js
@@ -1,22 +1,42 @@
-(function () {
-  let fs, fetch, pkg, vers;
-  fs = require('fs');
+(async function () {
+  // become grabby
+  let fs, fetch, pkg, vers, resp;
+  fs = require('fs').promises;
   fetch = require('node-fetch');
   pkg = require('./package.json');
-  fetch('https://cdn.jsdelivr.net/npm/utonmae@latest/package.json')
-  .then(resp => resp.json())
-  .then(info => {
-    vers = info.version.split('.').map(n => +n);
-    vers[vers.length - 1]++;
-    if (vers[vers.length - 1] > 9) {
-      vers[vers.length - 1] = 0;
-      vers[vers.length - 2]++
-    }
-    if (vers[vers.length - 2] > 9) {
-      vers[vers.length - 2] = 0;
-      vers[vers.length - 3]++
-    }
-    pkg.version = vers.join('.');
-    fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2), 'utf8')
-  })
+  
+  // le await the fetch request
+  resp = await fetch('https://cdn.jsdelivr.net/npm/utonmae@latest/package.json');
+  
+  // le await the response and grab it as a json format
+  resp = await resp.json();
+  
+  // convert the string into an array that can be easily edited
+  vers = resp.version.split('.').map(n => +n);
+  
+  //           V                   V
+  // if [1, 0, 0] is > 9; bump [1, 0, 0]
+  if (vers[2] > 9) {
+    vers[2] = 0;
+    vers[1]++
+  }
+  
+  //        V                   V
+  // if [1, 0, 0] is > 9; bump [1, 0, 0]
+  if (vers[1] > 9) {
+    vers[1] = 0;
+    vers[0]++
+  }
+  
+  // smmMOOSH back into 1.0.0 .w.
+  pkg.version = vers.join('.');
+  
+  // wait for fs to write ze fiel
+  let er = await fs.writeFile('./package.json', JSON.stringify(pkg, null, 2), 'utf8');
+  
+  // if uhoh- then die (lol I love the fact that inorder to throw an error in php.. you tell it to die())
+  if (er) {
+    console.log('Something went wrong.');
+    throw er;
+  }
 })();
